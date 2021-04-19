b3173c6e3c45e82cfa7ced71aa9814ad9824cb17
biodfrl89 <biodfrl@gmail.com>
2021-04-18 22:11:30 -0500

    Preparar ATGs de A. thaliana desde NCBI

---
+++ DATA/NCBI_wget_ppts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#Se descargan las tablas de proteínas de todos los cromosomas de A. thaliana
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_I/NC_003070.ptt
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_II/NC_003071.ptt
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_III/NC_003074.ptt
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_IV/NC_003075.ptt
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_V/NC_003076.ptt
