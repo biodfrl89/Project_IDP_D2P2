85a5a7a9e7aacc405a4199547f6e0c05d7347a7b
biodfrl89 <biodfrl@gmail.com>
2021-05-09 15:07:58 -0500

    Se elimina wget de NCBI y se opta por Araport11 CDS. Además se añaden instrucciones generales.

--- SCRIPTS/NCBI_wget_ppts.sh
+++ SCRIPTS/NCBI_wget_ppts.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+#Se necesita tener wget para descargar el fasta con los CDS de Araport11
+#Para instalarlo ejecuta:
 
-#Se descargan las tablas de proteínas de todos los cromosomas de A. thaliana
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_I/NC_003070.ptt
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_II/NC_003071.ptt
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_III/NC_003074.ptt
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_IV/NC_003075.ptt
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/archive/old_refseq/Arabidopsis_thaliana/CHR_V/NC_003076.ptt
+#sudo apt install wget
+
+###Aquí es necesario poner un candado. Si el archivo no existe donde se ejecuta
+###el archivo, se descarga. Si sí existe, se salta ese paso.
+
+#Se descargan el fasta con los CDS de Araport11
+wget https://www.arabidopsis.org/download/index-auto.jsp?dir=%2Fdownload_files%2FGenes%2FAraport11_genome_release%2FAraport11_blastsets
+
+###Hay que mostrar un mensaje que diga si existen codigos para los 5 cromosomas, 
+###así como secuencias de mitocondria y cloroplasto.
+
+###Hay que generar dos archivos un archivo donde venga el ATG, un tabulador y la longitud del CDS
+
+###El otro archivo, debe ser un fasta temporal, donde venga el header y como secuencia solamente
+###letras O, pero cuya secuencia debe ser de la longitud de cada secuencia. 
